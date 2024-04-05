b6adfc110bec37d36004b1f525fba061b6b02396
radudin11 <78203390+radudin11@users.noreply.github.com>
2024-04-05 11:52:46 +0300

    admin check done +

--- backend/routes/recipes.route.ts
+++ backend/routes/recipes.route.ts
@@ -3,10 +3,11 @@ import User from "../models/users.model";
 import Recipe from "../models/recipes.model";
 import bcrypt from "bcrypt";
 import validators from "../common/validators";
+import adminMiddleware from "../middlewares/adminMiddleware";
 
 const router = Router();
 
-router.get("/all-recipes", async (req, res) => {
+router.get("/all-recipes", adminMiddleware, async (req, res) => {
 	
 	// check if user is logged in
 	// check if user is admin
