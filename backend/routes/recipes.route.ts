2ccbbef19dd29f6a05d9c8c058f2b41769cd7971
Radu <rodede.radu@gmail.com>
2024-04-09 15:26:51 +0300

    simplified get review

--- backend/routes/recipes.route.ts
+++ backend/routes/recipes.route.ts
@@ -12,25 +12,8 @@ const pending = "PENDING";
 
 router.get("/review", adminMiddleware, async (req, res) => {
 	
-	// check if user is logged in
-	// check if user is admin
-	// return all recipes not made by admin
-
-	let recipes = await Recipe.find();
-	let recipesToSend = [];
-
-	for (let i = 0; i < recipes.length; i++) {
-		let user = await User.findById(recipes[i].userId);
-		if (!user) {
-			res.status(404).send("User not found");
-			return;
-		}
-		if (recipes[i].status != pending || recipes[i].reportNo < reportThreshold) {
-			continue;
-		}
-		recipesToSend.push(recipes[i]);
-	}
-	res.send(recipesToSend);
+	let recipes = await Recipe.find({ status: pending, reportNo: { $gte: reportThreshold } });
+	res.send(recipes);
 });
 
 
