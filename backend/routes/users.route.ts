d0522780698c49fb090c28dbc02154ec708cb496
Cosmina Mihoreanu <cosmina.mihoreanu@stud.acs.gmail.com>
2024-04-11 17:25:01 +0300

    added routes for viewing all users and removing user by id (as admin) + users.example

--- backend/routes/users.route.ts
+++ backend/routes/users.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import User from "../models/users.model";
 import bcrypt from "bcrypt";
 import validators from "../common/validators";
+import adminMiddleware from "../middlewares/adminMiddleware";
 
 const router = Router();
 
@@ -22,6 +23,23 @@ router.post("/login", async (req, res) => {
 		return res.status(404).send("User not found");
 	}
 	return res.send(user);
+});
+
+router.get("/view", adminMiddleware, async (req, res) => {
+	// send all users created in the system
+	const users = await User.find();
+	
+	return res.send(users);
+});
+
+router.delete("/remove/:id",adminMiddleware , async (req, res) => {
+	// remove user identified by "id" from database
+	const { id } = req.params;
+	const user = await User.findByIdAndDelete(id);
+	if (!user) {
+		return res.status(404).send("User not found");
+	}
+	return res.status(200).send("User deleted successfully");
 });
 
 export default router;
