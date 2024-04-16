9b6499fc76aa544ac20cd8ff1d08053461553b43
Cosmina Mihoreanu <cosmina.mihoreanu@stud.acs.gmail.com>
2024-04-16 12:53:47 +0300

    added try catch block

--- backend/routes/recipes.route.ts
+++ backend/routes/recipes.route.ts
@@ -22,29 +22,33 @@ router.get("/review", adminMiddleware, async (req, res) => {
 router.delete("/remove/:id", userMiddleware, async (req: any, res) => {
 	// delete recipe with given id if it belongs to the requesting user
 	const { id } = req.params;
-	let recipe = await Recipe.findById(id);
-	if (!recipe) {
-		return res.status(404).send("Recipe not found");
-	}
-	
-	if (!recipe.userId.equals(req.user._id)) {
-		return res.status(400).send("Unauthorized action");
-	}
-
-	// update user report no
-	await req.user.updateOne({ $inc: {reportNo: -recipe.reportNo} });
-	// delete reviews, reports and appearances in favorites and todo lists
-	await Review.deleteMany({ recipeId: id});
-	await Report.deleteMany({ reportedEntityId: id});
-	await User.updateMany({}, {
-		$pull: {
-			favoriteRecipes: id,
-			todoRecipes: id
+	try {
+		let recipe = await Recipe.findById(id);
+		if (!recipe) {
+			return res.status(404).send("Recipe not found");
 		}
-	});
+		
+		if (!recipe.userId.equals(req.user._id)) {
+			return res.status(400).send("Unauthorized action");
+		}
 
-	recipe.deleteOne();
-	return res.status(200).send("Recipe deleted successfully");
+		// update user report no
+		await req.user.updateOne({ $inc: {reportNo: -recipe.reportNo} });
+		// delete reviews, reports and appearances in favorites and todo lists
+		await Review.deleteMany({ recipeId: id});
+		await Report.deleteMany({ reportedEntityId: id});
+		await User.updateMany({}, {
+			$pull: {
+				favoriteRecipes: id,
+				todoRecipes: id
+			}
+		});
+
+		await recipe.deleteOne();
+		return res.status(200).send("Recipe deleted successfully");
+	} catch (e) {
+		return res.status(400).send("An error occured: " + e);
+	}
 });
 
 
