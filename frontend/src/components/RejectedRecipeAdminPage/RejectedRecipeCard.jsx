3ec135a1b9eca0942bda44e1ff85ce1f7e2e73b1
radudin11 <78203390+radudin11@users.noreply.github.com>
2024-05-15 17:25:53 +0300

    added popups

--- frontend/src/components/RejectedRecipeAdminPage/RejectedRecipeCard.jsx
+++ frontend/src/components/RejectedRecipeAdminPage/RejectedRecipeCard.jsx
@@ -1,13 +1,10 @@
-import { Box, SimpleGrid, GridItem, Text, Stack, HStack, IconButton, Image, useBreakpointValue, useToast } from '@chakra-ui/react';
-import { CheckIcon, CloseIcon } from '@chakra-ui/icons';
+import { Box, SimpleGrid, GridItem, Text, Stack, HStack, Image, useBreakpointValue } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import InfoButton from '../Buttons/InfoButton';
 
 function RecipeCard ({ recipeId, imageUrl, nameRecipe, reports, reportsOpen }){
   const stackDirection = useBreakpointValue({ base: 'column', md: 'row' });
   const navigate = useNavigate();
-  const toast = useToast();
 
   return (
     <Box bgColor='#F5F2F2' borderRadius='20' minH={{base:'15vh', md:'20vh'}} mb='1vh' w='98%'>
@@ -24,70 +21,10 @@ function RecipeCard ({ recipeId, imageUrl, nameRecipe, reports, reportsOpen }){
             <><HStack>
                 <InfoButton text='View Recipe' onClick={() => navigate(`/recipes/${recipeId}`)}/>
                 {reports > 0 && <InfoButton text='View Reports' onClick={() => reportsOpen(recipeId)}/>}
-                {/* <IconButton
-                  isRound={true}
-                  variant='solid'
-                  colorScheme='greenBrand'
-                  fontSize='20'
-                  icon={<CheckIcon />} 
-                  onClick={() => axios.post("http://localhost:5000/api/recipes/status", {
-                      id: recipeId,
-                      status: "APPROVED"
-                    }, {
-                      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
-                    }).then(() => {
-                      toast({
-                        title: 'Recipe approved.',
-                        description: 'The recipe has been approved.',
-                        status: 'success',
-                        duration: 5000,
-                        isClosable: true,
-                      });
-                      setTimeout(() => window.location.reload(), 1000);
-                    }).catch((error) => {
-                      toast({
-                        title: 'Error approving recipe.',
-                        description: error.response.data,
-                        status: 'error',
-                        duration: 5000,
-                        isClosable: true,
-                      });
-                    })}
-                  /> */}
               </HStack><Text fontSize={{ base: 'xs', md: 'md' }}>no. of reports: {reports}</Text></>
           ) : (
             <><HStack>
                   <Text fontSize={{ base: 'xs', md: 'md' }}>no. of reports: {reports}</Text>
-                  {/* <IconButton
-                    isRound={true}
-                    variant='solid'
-                    colorScheme='greenBrand'
-                    fontSize='20'
-                    icon={<CheckIcon />} 
-                    onClick={() => axios.post("http://localhost:5000/api/recipes/status", {
-                      id: recipeId,
-                      status: "APPROVED"
-                    }, {
-                      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
-                    }).then(() => {
-                      toast({
-                        title: 'Recipe approved.',
-                        description: 'The recipe has been approved.',
-                        status: 'success',
-                        duration: 5000,
-                        isClosable: true,
-                      });
-                      setTimeout(() => window.location.reload(), 1000);
-                    }).catch((error) => {
-                      toast({
-                        title: 'Error approving recipe.',
-                        description: error.response.data,
-                        status: 'error',
-                        duration: 5000,
-                        isClosable: true,
-                      });
-                    })}
-                  /> */}
                 </HStack>
                 <HStack mb="1vh">
                   <InfoButton size='sm' text='View Recipe' onClick={() => navigate(`/recipes/${recipeId}`)}/>
